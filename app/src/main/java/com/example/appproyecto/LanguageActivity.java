9a77455e28fa1e1c98136f2935346d52d8efab2b
Mikel Arina Marcos <mikel.arina@estudiantat.upc.edu>
2023-01-12 12:02:58 +0100

    Creada funcionalidad para cambiar idioma dispositivo de Android

--- app/src/main/java/com/example/appproyecto/LanguageActivity.java
+++ app/src/main/java/com/example/appproyecto/LanguageActivity.java
@@ -2,13 +2,35 @@ package com.example.appproyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LanguageActivity extends AppCompatActivity {
+
+    TextView txt_bienvenida;
+    Button btn_cambiar_idioma;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language);
+
+        txt_bienvenida = (TextView) findViewById(R.id.mensajeIdioma);
+        btn_cambiar_idioma = (Button) findViewById(R.id.button);
+
+        intent = new Intent(Intent.ACTION_MAIN);
+        intent.setClassName("com.android.settings", "com.android.settings.LanguageSettings");
+
+        btn_cambiar_idioma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(intent);
+            }
+        });
     }
 }
